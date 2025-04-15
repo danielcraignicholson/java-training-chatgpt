8849a90b9825ba05050e1b46ce6a4822fbfeae14
Daniel Nicholson <danielcraignicholson@outlook.com>
2025-04-15 15:07:54 -0400

    Completed student list assignment from chapter 3.

---
+++ src/main/java/com/github/danielcraignicholson/oop/student/Main.java
@@ -0,0 +1,18 @@
+package com.github.danielcraignicholson.oop.student;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StudentRegistry registry = new StudentRegistry();
+
+        registry.addStudent(List.of("Alice", "Bob", "Charlie"));
+        registry.removeStudent(List.of("Bob"));
+        registry.searchStudent("Charlie");
+        registry.searchStudent("Daniel");
+
+    }
+
+}
---
+++ src/main/java/com/github/danielcraignicholson/oop/student/StudentRegistry.java
@@ -0,0 +1,35 @@
+package com.github.danielcraignicholson.oop.student;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class StudentRegistry {
+
+    LinkedList<String> students = new LinkedList<>();
+
+    public void addStudent(List<String> newStudents) {
+        students.addAll(newStudents);
+        System.out.println("Adding students...");
+        displayStudents();
+    }
+
+    public void removeStudent(List<String> oldStudents) {
+        students.removeAll(oldStudents);
+        System.out.println("Removing..." + oldStudents);
+        displayStudents();
+    }
+
+    public void searchStudent(String student) {
+        System.out.println("Searching for " + student);
+        if (students.contains(student)) {
+            System.out.println("Found: " + student);
+        } else {
+            System.out.println("Not found: " + student);
+        }
+    }
+
+    public void displayStudents() {
+        System.out.println("Students: " + students);
+    }
+
+}
